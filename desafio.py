0eae98eb5794cf3469a0f22299fbf949167894dd
Rayssa Marconato <rayssa.g.marconato@gmail.com>
2025-04-23 20:19:49 -0300

    Add função escolher_opcoes_iniciais.

--- desafio.py
+++ desafio.py
@@ -16,6 +16,26 @@ def cadastrar_orientador():
     orientador = input("Digite o nome do professor orientador: ")
     orientadores.append({orientador: []})
 
+def escolher_opcoes_iniciais():
+    try:
+        opcao_escolhida = int(input('Escolha uma opção: '))
+        if opcao_escolhida == 1:
+            cadastrar_orientador()
+        elif opcao_escolhida == 2:
+            print()
+            # cadastrar_aluno()
+        elif opcao_escolhida == 3:
+            print()
+            # mostrar_opcoes()
+        elif opcao_escolhida == "q":
+            print()
+            # sair()
+        else:
+            print()
+            # opcao_invalida()
+    except:
+        #  opcao_invalida()
+
 # cadastrar_orientador()
 # cadastrar_orientador()
 # print(orientadores)
