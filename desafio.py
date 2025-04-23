6eda2fb648be7eedc6a21b2dc86ebb8a0a717571
ma-pedroza <matheusgomeszpedroza@gmail.com>
2025-04-23 20:41:36 -0300

    Comitt Matheus - Inicial
    Add um escopozinho da opção 3 do menu

--- desafio.py
+++ desafio.py
@@ -16,6 +16,48 @@ def cadastrar_orientador():
     orientador = input("Digite o nome do professor orientador: ")
     orientadores.append({orientador: []})
 
+def funcionalidades():
+    print(
+        f"""
+        Digite uma das opçoes:
+
+1 - Registrar nova entrega.
+2 - Registrar nota.
+3 - Listar alunos por orientador.
+4 - Listar versões entregues por aluno.
+5 - Listar pendências de avaliação.
+6 - Gerar relatório do orientador.
+7 - Voltar ao menu principal.
+       """
+    )
+    opcao_funcionalidade = input(": ")
+
+    match opcao_funcionalidade:
+        case 1:
+            # Registrar nova entrega.
+            return none
+        case 2:
+            # Registrar nota.
+            return none
+        case 3:
+            # Listar alunos por orientador.
+            return none
+        case 4:
+            # Listar versões entregues por aluno.
+            return none
+        case 5:
+            # Listar pendências de avaliação.
+            return none
+        case 6:
+            # Gerar relatório do orientador.
+            return none
+        case 7:
+            # Voltar ao menu principal.
+            return none
+        case _:
+            print("Default") 
+            return none
+
 def escolher_opcoes_iniciais():
     try:
         opcao_escolhida = int(input('Escolha uma opção: '))
