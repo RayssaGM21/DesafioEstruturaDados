73f1cc73a1a9e0d2da71e247e2a0a36627470ca7
Rayssa Marconato <rayssa.g.marconato@gmail.com>
2025-04-23 19:57:07 -0300

    Commit inicial

---
+++ desafio.py
@@ -0,0 +1,16 @@
+orientadores = []
+alunos = []
+
+def exibir_menu():
+    print(
+        f"""
+        1. Cadastrar alunos e orientadores.\n
+        2. Registro de versões do TCC entregues.\n
+        3. Atribuição de notas por parte dos orientadores.\n
+        4. Verificação de pendências de entregas por aluno.\n
+        5. Relatórios para o orientador.\n
+        """
+    )
+    
+def cadastrar_orientador():
+    pass
