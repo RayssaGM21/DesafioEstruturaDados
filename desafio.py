3f6b699cefc1ae68fab02bb865bafdbb7156c928
Rayssa Marconato <rayssa.g.marconato@gmail.com>
2025-04-23 20:56:32 -0300

    Ajustes.

--- desafio.py
+++ desafio.py
@@ -4,27 +4,31 @@ alunos = []
 def exibir_menu():
     print(
         f"""
-        1. Cadastrar alunos e orientadores.\n
-        2. Registro de versões do TCC entregues.\n
-        3. Atribuição de notas por parte dos orientadores.\n
-        4. Verificação de pendências de entregas por aluno.\n
-        5. Relatórios para o orientador.\n
+        1. Cadastrar alunos.\n
+        2. Cadastrar orientadores.\n
+        3. Realizar operações.\n
         """
     )
     
 def cadastrar_orientador():
     orientador = input("Digite o nome do professor orientador: ")
     orientadores.append({orientador: []})
+    
+def registrar_entrega():
+    pass
+
+def registrar_nota():
+    pass
 
 def escolher_opcoes_iniciais():
     try:
-        opcao_escolhida = int(input('Escolha uma opção: '))
-        if opcao_escolhida == 1:
+        opcao_escolhida = input('Escolha uma opção: ')
+        if opcao_escolhida == "1":
             cadastrar_orientador()
-        elif opcao_escolhida == 2:
+        elif opcao_escolhida == "2":
             print()
             # cadastrar_aluno()
-        elif opcao_escolhida == 3:
+        elif opcao_escolhida == "3":
             print()
             # mostrar_opcoes()
         elif opcao_escolhida == "q":
